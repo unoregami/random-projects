c39a11449855c181ea8b5bc22d40c2ef93e31782
aesparas <adolfoesparas@gmail.com>
2025-12-29 17:36:48 +0800

    chore: added bingo gamemodes
    - added blackout, corner, and custom number of bolas
    - added custom bingo cards simulation
    - added catch for edge cases

--- bingo.py
+++ bingo.py
@@ -10,6 +10,9 @@ def reloadBola(bola):   # reset bola
     return bola
 
 def bunotBola(bola):    # bunot bola and removes num from choices
+    if len(bola) <= 0:  # catches if bola is empty
+        return 0
+
     print("Bunoting...")
     # time.sleep(3)
     choice = random.choice(bola)
@@ -76,45 +79,99 @@ def checkCardForNum(card, bunot):  # check bunot to bingo card
                 return card
     return card
 
-def isBingo(card): # checks card if bingo
+def isWin(card, bingo=True, blackout=False, corner=False, numBola=0): # checks card if win
+    if numBola < 0:     # catches numBola out of bounds and normalize
+        numBola = 0
+    elif numBola > 25:
+        numBola = 25
+    output = None
     starNumIndex = []
     for letter in card:
         for i in range(len(card[letter])):
             if card[letter][i] == "★":
                 starNumIndex.append(i)
-    # Horizontal Bingo
-    for i in range(5):
-        bingo = starNumIndex.count(i)
-        if bingo == 5:
+    def isBingo():
+        # Horizontal Bingo
+        for i in range(5):
+            bingo = starNumIndex.count(i)
+            if bingo == 5:
+                return True
+        # Slanted Bingo
+        if card["B"][0] == "★" and card["I"][1] == "★" and card["N"][2] == "★" and card["G"][3] == "★" and card["O"][4] == "★":
             return True
+        if card["B"][4] == "★" and card["I"][3] == "★" and card["N"][2] == "★" and card["G"][1] == "★" and card["O"][0] == "★":
+            return True
+        return False
+
+    def isBlackout():   # blackout bingo gamemode
+        if len(starNumIndex) == 25:
+            return True
+        return False
+
+    def isCorner():     # corner gamemode
+        if card["B"][0] == "★" and card["B"][4] == "★" and card["O"][0] == "★" and card["O"][4] == "★":
+            return True
+        return False
     
-    # Slanted Bingo
-    if card["B"][0] == "★" and card["I"][1] == "★" and card["N"][2] == "★" and card["G"][3] == "★" and card["O"][4] == "★":
-        return True
-    if card["B"][4] == "★" and card["I"][3] == "★" and card["N"][2] == "★" and card["G"][1] == "★" and card["O"][0] == "★":
-        return True
+    def isNumBola():    # number of bola gamemode
+        if len(starNumIndex) >= numBola:
+            return True
+        return False
     
-    return False
+    if bingo:
+        if output == None:
+            output = isBingo()  # initializes the status of output
+    if blackout:
+        if output == None:
+            output = isBlackout()
+        elif output and isBlackout():   # combines gamemodes condition
+            output = True
+        else:
+            output = False
+    if corner:
+        if output == None:
+            output = isCorner()
+        elif output and isCorner():
+            output = True
+        else:
+            output = False
+    if numBola:
+        if output == None:
+            output = isNumBola()
+        elif output and isNumBola():
+            output = True
+        else:
+            output = False
+    
+    if output == None:      # catches if every gamemode is false
+        output = False
+    return output
 
-def isBlackout(card):   # checks if card is blackout bingo
-    pass
+def simulateBingo(numOfCards:int, bingo=True, blackout=False, corner=False, numBola=0):  # simulate bingo game with custom number of bingo cards
+    cards = {}
+    for i in range(numOfCards):
+        card = populateCard()
+        cards[f"CARD {i+1}"] = card
+    bunotCount = 0
 
-
-bola = reloadBola(bola)
-
-card = populateCard()
-displayCard(card)
-bunotCount = 0
-while True:
-    bunot = bunotBola(bola)
-    bunotCount += 1
-    card = checkCardForNum(card, bunot)
-    displayCard(card)
-    if isBingo(card):
-        print("""▄▄▄▄▄▄▄   ▄▄▄▄▄ ▄▄▄    ▄▄▄  ▄▄▄▄▄▄▄    ▄▄▄▄▄    ▄▄ 
+    while True:
+        bunot = bunotBola(bola)
+        if bunot <= 0:      # catches if bola is empty
+            break
+        bunotCount += 1
+        for card in cards:
+            cards[card] = checkCardForNum(cards[card], bunot)
+            if isWin(cards[card], bingo=bingo, blackout=blackout, corner=corner, numBola=numBola):
+                print(card)
+                displayCard(cards[card])
+                print("""▄▄▄▄▄▄▄   ▄▄▄▄▄ ▄▄▄    ▄▄▄  ▄▄▄▄▄▄▄    ▄▄▄▄▄    ▄▄ 
 ███▀▀███▄  ███  ████▄  ███ ███▀▀▀▀▀  ▄███████▄  ██ 
 ███▄▄███▀  ███  ███▀██▄███ ███       ███   ███  ██ 
 ███  ███▄  ███  ███  ▀████ ███  ███▀ ███▄▄▄███  ▀▀ 
 ████████▀ ▄███▄ ███    ███ ▀██████▀   ▀█████▀   ██ """)
-        print("Bunot Count:", bunotCount)
-        break
+                print("Bunot Count:", bunotCount)
+                return
+
+
+bola = reloadBola(bola)
+simulateBingo(1000)
